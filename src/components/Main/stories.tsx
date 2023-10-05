711583268125c5bdf85ca10c973a2ed5551af1b4
Marcos Domingues <marcos.domingues@poncetech.com.br>
2023-10-05 11:36:11 -0300

    (config) strories

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -4,9 +4,17 @@ import Main from '.'
 export default {
   title: 'components/Main',
   component: Main,
+
   parameters: {
     layout: 'fullscreen'
   }
 } as Meta
 
 export const Default: StoryObj = {}
+
+export const Basic: StoryObj = {
+  args: {
+    title: 'title basic',
+    description: 'description basic'
+  }
+}
